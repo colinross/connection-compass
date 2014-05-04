911592c8a49d3c4c8069ecb175c47497fe71a09a
Chum <colinross@gmail.com>
2014-05-03 20:08:38 -0700

    Back to multiple envs in the settings file; Remove constants in Services::Facebook in lue of using the settings object

--- lib/services/facebook.rb
+++ lib/services/facebook.rb
@@ -3,22 +3,16 @@ require_relative 'base'
 module Services
   # Usage: 
   # fb_service = Services::Facebook.new(users_facebook_access_token)
-  # fb_service.get_friends  # get hash of friends
-  # fb_service.get "/me"
   class Facebook < Base
     # Not directly needed now, but eventualy we should sign all our fb requests with the hash of these,
     # see https://developers.facebook.com/docs/graph-api/securing-requests/
-    # make this a call to the settings file without having to include
-    # the whole app
-    APP_ID = "754336197924702"
-    APP_SECRET = "9d1ccd0bed4b0978bea7df6b6b689481"
-    APP_ACCESS_TOKEN = "754336197924702|2MRBX58xoXD33FNmtmcsa_ZteyE"
+     
     attr_accessor :access_token
 
     # CLASS Methods
     class <<self 
       def _app_api_client
-        @@app_api_client ||= new(APP_ACCESS_TOKEN)
+        @@app_api_client ||= new(::ConnectionCompass.settings.services["facebook"]["app_access_token"])
       end
       def fb_object_cache_store
         @@fb_object_cache_store ||= Moneta.new(:DataMapper, setup: ConnectionCompass::DATABASE_URL, table: :fb_object_cache)
